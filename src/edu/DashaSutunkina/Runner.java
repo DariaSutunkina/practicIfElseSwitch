a7cf5b1e304e9eb9ae3b8802355da8425ccb0086
Anastasiia Tkachuk <anastaisha.580@gmail.com>
2023-01-27 11:18:38 +0200

    rename

--- src/edu/DashaSutunkina/Runner.java
+++ src/edu/DashaSutunkina/Runner.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int mouth = scanner.nextInt();
-        if (mouth <= 2 || mouth == 12) {
+        int month = scanner.nextInt();
+        if (month <= 2 || month == 12) {
             System.out.println("this is winter");
-        } else if (mouth <= 5) {
+        } else if (month <= 5) {
             System.out.println("this is spring");
-        } else if (mouth <= 8) {
+        } else if (month <= 8) {
             System.out.println("this is summer");
-        } else if (mouth <= 11) {
+        } else if (month <= 11) {
             System.out.println("this is autumn");
         } else {
             System.out.println("error");
